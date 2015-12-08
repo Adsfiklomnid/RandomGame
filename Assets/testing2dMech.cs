b9a1b8b19318bee315ea74066cb162b6582555da
ciaran3001 <ciarankelly15@hotmail.com>
2015-12-08 19:49:28 +0000

    basic code

---
+++ Assets/testing2dMech.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testing2dMech : MonoBehaviour {
+
+
+	public GameObject sprite;
+	
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown ("space")){
+		print("jump");
+			sprite.rigidbody2D.AddForce(Vector2.up * 500);
+		}
+		
+		if(Input.GetKeyDown ("a")){
+		print("a");
+			sprite.rigidbody2D.AddForce(Vector2.right * -50);
+		}
+	}
+}
